0f32889089f913c79c8f0a6244fb6167e47ea548
JKleinCalPoly <jpklein@calpoly.edu>
2022-05-12 16:02:44 -0700

    Roadmap

--- memSim.py
+++ memSim.py
@@ -31,10 +31,29 @@ def read_args():
                 exit(1)
     return inputfile, frames, algo
 
+def TLB_lookup(TLB, page_num):
+    for pnum, fnum in TLB:
+        if pnum == page_num:
+            return fnum
+    return None
+
 
 if __name__ == '__main__':
     file, num_frames, algo = read_args()
     print("simulating %d frames from %s with %s" % (num_frames, file, algo))
     addrs = read_file(file)
-    for i, addr in enumerate(addrs):
-        print("%d: %s" % (i, addr))
+    #for i, addr in enumerate(addrs):
+    #    print("%d: %s" % (i, addr))
+    TLB = []
+    for addr in addrs:
+        page_num = int(addr) & 0xFF00
+        fnum = TLB_lookup(TLB, page_num)
+        if fnum is None:
+            #tlbmiss increment
+            #page table lookup
+            if len(TLB) >= 16:
+                TLB.pop(0)
+            TLB.append((page_num, 0))
+        #get target byte + frame
+        #print info
+    #print hit and fault rates
